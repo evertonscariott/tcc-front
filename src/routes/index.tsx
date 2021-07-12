d82f71148b0ab515d84bd3541f3bcbac7dfca575
Everton <evertonscariott@gmail.com>
2021-07-12 20:12:22 -0300

    Created SprintFrames

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import Dashboard from "../pages/dashboard";
+import SprintFrames from "../pages/SprintFrames";
 
 export default function Routes() {
     return (
         <Router>
             <Switch>
-                <Dashboard />
+                {/* <Dashboard /> */}
+                <SprintFrames />
             </Switch>
         </Router>
     );
